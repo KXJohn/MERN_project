82a35e66dd882d75aac28133ec41b9477cd6e42c
KaixiangJony <kaixiangjohn@gmail.com>
2025-02-24 21:44:34 +1030

    header WIP

---
+++ frontend/mern_frontend/src/shared/components/Navigation/MainNavigation.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+
+import { MainHeader } from "./MainHeader.tsx";
+import "./MainNavigation.css";
+
+export const MainNavigation: FC = () => {
+  return (
+    <MainHeader>
+      <button className="main-navigation__menu-btn">
+        <span />
+        <span />
+        <span />
+      </button>
+      <h1 className="main-navigation__title">
+        <Link to="/">YourPlaces</Link>
+      </h1>
+      <nav>...</nav>
+    </MainHeader>
+  );
+};
--- frontend/mern_frontend/src/users/components/UsersList.tsx
+++ frontend/mern_frontend/src/users/components/UsersList.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { Users } from "./types.ts";
 import { UserListItem } from "./UserListItem.tsx";
 import { UserListContainer } from "./style.ts";
+import { Card } from "../../shared/components/UIElements/Card.tsx";
 
 interface UsersListProps {
   users: ReadonlyArray<Users>;
@@ -9,7 +10,13 @@ interface UsersListProps {
 
 export const UsersList: FC<UsersListProps> = ({ users }) => {
   if (users.length === 0) {
-    return <div />;
+    return (
+      <div>
+        <Card>
+          <h2>No User Found</h2>
+        </Card>
+      </div>
+    );
   }
 
   return (
