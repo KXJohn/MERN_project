3c64576f065a2dca3789923fdc7b4b5992fd58a9
KaixiangJony <kaixiangjohn@gmail.com>
2025-02-24 21:46:39 +1030

    WIP

--- frontend/mern_frontend/src/App.tsx
+++ frontend/mern_frontend/src/App.tsx
@@ -3,10 +3,12 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import { User } from "./users/pages/Users.tsx";
 import { Fragment } from "react";
 import { NewPlace } from "./places/pages/Places.tsx";
+import { MainNavigation } from "./shared/components/Navigation/MainNavigation.tsx";
 
 function App() {
   return (
     <Fragment>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<User />} />
         <Route path="/places/new" element={<NewPlace />} />
