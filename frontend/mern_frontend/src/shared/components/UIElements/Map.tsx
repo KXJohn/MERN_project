8ca1322d149e0d19f62d86b813ce3dd9841a4935
KaixiangJony <kaixiangjohn@gmail.com>
2025-03-02 21:11:22 +1030

    show map and add marker on the map

--- frontend/mern_frontend/src/shared/components/UIElements/Map.tsx
+++ frontend/mern_frontend/src/shared/components/UIElements/Map.tsx
@@ -1,25 +1,46 @@
 import { CSSProperties, FC } from "react";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import {
+  AdvancedMarker,
+  APIProvider,
+  Map,
+  Pin,
+} from "@vis.gl/react-google-maps";
+import { API_KEY, MAP_ID } from "../../key.ts";
 
 interface MapProps {
+  key?: string;
   className?: string;
   style?: CSSProperties;
   zoom?: number;
   center: google.maps.LatLngLiteral;
 }
 
-const API_KEY = "#";
-
-export const MapDisplay: FC<MapProps> = ({ style, center, zoom }) => {
+export const MapDisplay: FC<MapProps> = ({ key, style, center, zoom }) => {
   return (
     <APIProvider apiKey={API_KEY}>
       <Map
+        mapId={MAP_ID}
         style={style}
         defaultCenter={center}
         defaultZoom={zoom}
         gestureHandling={"greedy"}
         disableDefaultUI={false}
-      />
+      >
+        <PoiMakers key={key} center={center} />
+      </Map>
     </APIProvider>
+  );
+};
+
+interface PoiMakersProps {
+  key?: string;
+  center: google.maps.LatLngLiteral;
+}
+
+const PoiMakers: FC<PoiMakersProps> = ({ key, center }) => {
+  return (
+    <AdvancedMarker key={key} position={center}>
+      <Pin glyphColor={"#000"} borderColor={"#000"} />
+    </AdvancedMarker>
   );
 };
