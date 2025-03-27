8ceeb2b613b92bb65221e4e4c321dac93b7034c9
KaixiangJony <kaixiangjohn@gmail.com>
2025-03-27 21:16:29 +1030

    add new route

--- backend/src/routes/place-route.ts
+++ backend/src/routes/place-route.ts
@@ -2,10 +2,36 @@ import express, { Express, Request, Response } from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const placeToReturn = DUMMY_PLACES.find((place) => place.id === placeId);
   console.log("GET Request");
 
-  res.json({ message: "Get Request" });
+  res.json({ place: placeToReturn });
+});
+
+router.get("/:id", (req, res, next) => {
+  const userId = req.params.id;
+
+  const place = DUMMY_PLACES.find((place) => place.id === userId);
+
+  res.json({ place });
 });
 
 export default router;
